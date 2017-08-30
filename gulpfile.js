3d2431ed63f4bc5cb936fd004a6a232aaa7628a3
shenyuancheng <shenyuanchen@mail.xusenintl.com>
2017-08-30 11:27:33 +0800

    gulp-server-livereload换成gulp-webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var webserver = require('gulp-webserver');
 var uglify = require('gulp-uglify');
 var uglifyCss = require('gulp-uglifycss');
 
-
 gulp.task('webserver', function() {
-	gulp.src('')
-		.pipe(server({
+    gulp.src('')
+        .pipe(webserver({
             host: '10.52.30.105',
-			port: '9000',
-			livereload: true,
+            port: '9000',
+            livereload: true,
             open: true,
-            defaultFile: 'index.html'
-		}));
+            fallback: 'index.html'
+        }));
 });
+// gulp.task('webserver', function() {
+// 	gulp.src('')
+// 		.pipe(server({
+//             host: '10.52.30.105',
+// 			port: '9000',
+// 			livereload: true,
+//             open: true,
+//             defaultFile: 'index.html'
+// 		}));
+// });
 
 gulp.task("buildHtml", function() { 
-	gulp.src(['index.html', 'invite.html'])
+	gulp.src(['index.html'])
         .pipe(gulp.dest('dist'));
 });
 gulp.task("buildCss", function() {
@@ -37,7 +47,11 @@ gulp.task("buildAudio", function() {
     gulp.src('audio/**')
         .pipe(gulp.dest('dist/audio'));
 });
+gulp.task("buildFont", function() {
+    gulp.src('font/**')
+        .pipe(gulp.dest('dist/font'));
+});
 
-gulp.task('build', ['buildHtml', 'buildCss', 'buildJs', 'buildImages', 'buildAudio'], function(e) {
+gulp.task('build', ['buildHtml', 'buildCss', 'buildJs', 'buildImages', 'buildAudio', 'buildFont'], function(e) {
     console.log(e);
 });
